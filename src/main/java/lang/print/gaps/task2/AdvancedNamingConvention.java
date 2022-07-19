4d05a88074f52ef0413c0a0620cc65ac37addde6
Nishanov Khasan <99598107+HasanNishanov@users.noreply.github.com>
2022-07-19 13:44:53 +0500

    finished the task

---
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -0,0 +1,16 @@
+package lang.print.gaps.task2;
+
+public class AdvancedNamingConvention {
+    private static final int ADULT_AGE = 18;
+
+    private int age;
+    private int phoneNumber;
+
+    void callTofriend() {
+        //some code
+    }
+
+    void callBynumber(int number) {
+        //some code
+    }
+}
--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -2,13 +2,13 @@ package lang.print.gaps.task2;
 
 public class CopiedNames {
     public static void main(String[] args) {
-        int variable = 0;
-        int variable = 2000;
-        int value = 1000;
-        int variable = 4000;
-        int value2 = 3000;
-        int variables = 5000;
-        variables = 7000;
+        int id = 0;
+        int phone = 2000;
+        int money = 1000;
+        int box = 4000;
+        int date = 3000;
+        int score = 5000;
+        int java = 7000;
         int value = 90000;
     }
 }
